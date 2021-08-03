94ab789cda703559ece140526a3c93d13eb6e087
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-08-03 17:23:15 -0300

    final

--- src/app/room.ts
+++ src/app/room.ts
@@ -1,9 +1,17 @@
 export class Room{
-  id!: number
-  name!: string
-  date!: string
-  startHour!: string
-  endHour!: string
-  active!: boolean
+  id: number
+  name: string
+  date: string
+  startHour: string
+  endHour: string
+  active: boolean
 
+  // constructor(room: Room){
+  //   this.id = room.id;
+  //   this.name = room.name;
+  //   this.date = room.date;
+  //   this.startHour = room.startHour;
+  //   this.endHour = room.endHour;
+  //   this.active = room.active;
+  // }
 }
