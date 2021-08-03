8bafc8d2aa5685c1d54db9f02a5459b856552159
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-08-03 16:48:17 -0300

    update. room component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { RoomListComponent } from './room-list/room-list.component';
 import { RoomDetailsComponent } from './room-details/room-details.component';
 import { UpdateRoomComponent } from './update-room/update-room.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoomListComponent,
     RoomDetailsComponent,
-    UpdateRoomComponent
+    UpdateRoomComponent,
+    CreateRoomComponent
   ],
   imports: [
     BrowserModule
