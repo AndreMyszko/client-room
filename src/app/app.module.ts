c702ef54d6fc2114f08159e89334079600111807
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-08-03 16:10:08 -0300

    update. basic structure with service folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomListComponent,
+    RoomDetailsComponent,
+    UpdateRoomComponent
   ],
   imports: [
     BrowserModule
